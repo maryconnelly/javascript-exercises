a8eec382cd40709237e937f192d5fc43c5ac6974
maryconnelly <maryconnelly14@gmail.com>
2024-10-08 21:51:31 -0500

    Pass 7 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || isNaN(start) || isNaN(end) || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    };
+    if (start > end) {
+        let finalSum = 0;
+    for (i=end; i <= start; i++) {
+        finalSum += i;
+    }
+return finalSum;
+    } else {
+    let finalSum = 0;
+    for (i=start; i <= end; i++) {
+        finalSum += i;
+    }
+return finalSum;
+}};
 
 // Do not edit below this line
 module.exports = sumAll;
