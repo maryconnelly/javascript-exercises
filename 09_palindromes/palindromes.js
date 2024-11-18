eec5e0f78dbf6be4e33256c15b23e32d10601da6
Mary Connelly <maryconnelly14@gmail.com>
2024-11-18 16:11:11 -0600

    passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const cleanedString = string
+.toLowerCase()
+.slice()
+//new string
+.split('')
+//this gives me s t r i n g
+.filter((character) => alphanumerical.includes(character))
+//makes sure numbers and letters are accepted
+.join('');
+
+const reversedString = cleanedString.split('').reverse().join('');
+
+
+return cleanedString === reversedString;
 
 };
 
