e29d15cdaa5d697aeb45800e03327488536e4673
Mary Connelly <maryconnelly14@gmail.com>
2024-11-18 20:50:41 -0600

    Function to make fib sequence works

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+//const fibonacci = function(member) {
 
+
+let fibArray = [1, 1, 2, 3, 5, 8];
+
+for (i = 0; i <= 25; i++) {
+    let newMember = fibArray.at(-2) + fibArray.at(-1);
+    fibArray.push(newMember);
 };
 
+//find nth member of fib array
+//let index = fibArray.at(member - 1);
+//return member;
+
+//};
+
+console.log(fibArray);
+
+ 
 // Do not edit below this line
-module.exports = fibonacci;
+//module.exports = fibonacci;
